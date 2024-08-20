eb3b266db6340cd1a4f9beafbb6924b2fe2ac369
Vishal Prakash <prakashvishal93@gmail.com>
2024-08-21 04:21:18 +0530

    created todo

--- Week5/counter-app/src/App.jsx
+++ Week5/counter-app/src/App.jsx
@@ -3,15 +3,53 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+     title : "Go to Gym",
+     description : "Go to Gym form 7",
+     completed  : "false"
+    },
+    {
+     title : "Study DSA",
+     description : "Study DSA From 11-12",
+     completed : "true"
+    }
+  ]);
+
+
+  function addTodo(){
+    setTodos([...todos,{
+      title : "new todo ",
+      description : "new Desc"
+    }])
+        
+  }
+
 
   return (
     <div>
-      Hi There
       
+      <button onClick={addTodo}>Add a Random Todo</button>
+
+      {todos.map(function(todo){
+        return <Todo title = {todo.title} description = {todo.description} />
+      })}
     </div>
   )
+}
+
+
+function Todo(props){
+  return (
+    <div>
+    <h1>{props.title}</h1>
+    <h1>{props.description}</h1>
+  </div>
+  )
+
 }
 
 export default App
