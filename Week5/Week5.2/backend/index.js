c4b3778e966876b657fcf9a7682ebe208ef13564
Vishal Prakash <prakashvishal93@gmail.com>
2024-08-21 06:15:27 +0530

    schema of zod created

--- Week5/Week5.2/backend/index.js
+++ Week5/Week5.2/backend/index.js
@@ -1,15 +1,34 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./type');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todo', function(req,res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "You send the wrong input"
+        })
+        return;
+    }
+    // we can also put this in mongodb 
 
 })
 app.get('/todos', function(req,res){
 
 })
 app.put('/completed', function(req,res){
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg : "You send the wrong input"
+        })
+        return;
+    }
+
 
 })
 
